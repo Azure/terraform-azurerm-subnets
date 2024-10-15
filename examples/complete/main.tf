da1c9bcd388fb4205174794414f29a77403b7a58
sol-stiep <sol.stiep@southworks.com>
2024-10-15 12:30:58 +0200

    Add outbound access field to subnet

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -62,6 +62,7 @@ module "vnet" {
     }
     subnet2 = {
       address_prefixes = ["10.0.2.0/24"]
+      default_outbound_access_enabled = false
       nat_gateway = {
         id = azurerm_nat_gateway.example.id
       }
